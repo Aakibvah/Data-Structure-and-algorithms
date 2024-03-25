f3e7e4e48e385c6773dc7b1b7ecb7d0084d317cc
Aakibvah <87711863+Aakibvah@users.noreply.github.com>
2024-03-25 17:01:37 -0400

    soln added for printing sub arrays

---
+++ Arrays/BasicPrintingSubArrays.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int s = i; s <= j; s++)
+            {
+                cout << arr[s] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
